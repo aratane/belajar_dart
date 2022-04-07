55ec747e80f58a9a8170808362d5af06d37a9d95
aratane <akunbotak503@gmail.com>
2022-04-07 17:59:16 +0700

    Pemula

---
+++ bin/belajar_dart.dart
@@ -0,0 +1,115 @@
+import 'dart:io';
+
+void main() {
+  /*
+  var buka = 8;
+  var tutup = 21;
+  var sekarang = 17;
+
+  if (sekarang > buka && sekarang < tutup) {
+    print('Hallo, Kami sekarang buka');
+  } else {
+    print('Maaf, saat ini kami tutup');
+  }
+
+  int x,y;
+  for (x = 0; x <=7; x++) {
+    print('');
+
+    for (y = 0; y < x; y++) {
+      print(" * ");
+    }
+  }
+
+  var i = 1;
+  while (i <= 100) {
+    print(i);
+    i++;
+  }
+
+  do {
+    print(i);
+    i++;
+  } while (i <= 100);
+
+  String username;
+  bool notValid = false;
+
+  do {
+    stdout.write('Masukkan nama Anda (min. 6 karakter) : ');
+    username = stdin.readLineSync()!;
+
+    if (username.length < 6) {
+      notValid = true;
+      print('Username Anda tidak valid');
+    } else {
+      print('Username Valid');
+      print('Selamat datang !!!');
+      break;
+    }
+  } while (notValid);
+
+  var nomorPrima = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71];
+  stdout.write('Masukkan bilangan prima : ');
+  var cariNomor = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < nomorPrima.length; i++) {
+    if (cariNomor == nomorPrima[i]) {
+      print('$cariNomor adalah bilangan prima ke-${i+1}');
+      break;
+    }
+    print('$cariNomor != ${nomorPrima[i]}');
+  }
+
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+      continue;
+    }
+    print(i);
+  }
+
+  stdout.write('Masukkan angka pertama : ');
+  var pertama = num.parse(stdin.readLineSync()!);
+  stdout.write('Masukkan operator [ + | - | * | / ] : ');
+  var operator = stdin.readLineSync();
+  stdout.write('Masukkan angka kedua : ');
+  var kedua = num.parse(stdin.readLineSync()!);
+
+  switch (operator) {
+    case '+':
+      print('$pertama $operator $kedua = ${pertama + kedua}');
+      break;
+    case '-':
+      print('$pertama $operator $kedua = ${pertama - kedua}');
+      break;
+    case '*':
+      print('$pertama $operator $kedua = ${pertama * kedua}');
+      break;
+    case '/':
+      print('$pertama $operator $kedua = ${pertama / kedua}');
+      break;
+    default:
+      print('Operator tidak ditemukan !!');
+  }
+
+  var i = 3;
+  while (i > 0) {
+    print(i);
+    i--;
+  }
+  print('Selesai');
+
+  var kesukaan = ['Seblak', 'Pizza', 'Cilok', 'Bakso'];
+  var lainnya = ['Apel', 'Mangga', 'Jeruk'];
+  var allFavorite = [...kesukaan, ...lainnya];
+  print(allFavorite);
+
+  var list;
+  var list2 = [0, ...?list];
+  print(list2);
+   */
+
+
+}
+
+
