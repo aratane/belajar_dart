02801e9ed5caedadbc7527f357010e658987fabb
aratane <akunbotak503@gmail.com>
2022-04-07 19:08:51 +0700

    up

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -107,9 +107,42 @@ void main() {
   var list;
   var list2 = [0, ...?list];
   print(list2);
+
+  var numberSet = {1, 4, 6};
+  Set<int> anotherSet = new Set.from([1, 4, 6, 4, 1]);
+  print(anotherSet);
+
+  numberSet.add(6);
+  numberSet.addAll({2, 2, 3});
+  print(numberSet);
+
+  numberSet.remove(3);
+  print(numberSet);
+
+  print(numberSet.elementAt(2));
+
+  var setA = {1, 2, 4, 5};
+  var setB = {1, 5, 7};
+
+  var union = setA.union(setB);
+  var intersection = setA.intersection(setB);
+
+  print(union);
+  print(intersection);
+
+  var ibukota = {
+    'Jakarta': 'Indonesia',
+    'London': 'England',
+    'Tokyo': 'Japan'
+  };
+  print(ibukota['Jakarta']);
+
+  var mapKeys = ibukota.keys;
+  var mapValue = ibukota.values;
+
+  ibukota['New Delhi']= 'India';
+  print(ibukota);
    */
-
-
 }
 
 
